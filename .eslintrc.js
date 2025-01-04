49dee8d222d575b1f650b31108607665a92e76ea
Allen Yeh <allen_yeh@systemweb.com.tw>
2025-01-04 23:41:27 +0800

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
