c3829c876c65afd23971233626a87acced11dacb
Allen Yeh <allen_yeh@systemweb.com.tw>
2025-01-08 20:04:41 +0800

    新增遊戲計時

--- src/views/game.vue
+++ src/views/game.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="gm">
+    <p class="">{{ timeText }}</p>
     <div class="">
       <ol v-if="gameQuestions">
         <li v-for="quest in gameQuestions">
@@ -11,9 +12,7 @@
       </div>
     </div>
     <div class="gm__buttons">
-      <Button v-if="!gameQuestions" @click="getRandomQuestions"
-        >支援前線</Button
-      >
+      <Button v-if="!gameQuestions" @click="clickGameStart">支援前線</Button>
       <router-link v-else :to="{ name: 'score' }">
         <Button severity="info">結算</Button>
       </router-link>
@@ -22,14 +21,39 @@
 </template>
 <script setup lang="ts">
 import gameStore from '~me/stores/gameStore'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { sampleSize, map, random } from 'lodash-es'
 
 const GAME_QUESTION_COUNT = 3
 
+const GAME_LIMIT_SECONDS = 180
+
 const gameQuestions = ref<
   { name: string; unit: string; count: number }[] | null
 >(null)
+
+const gameTimer = ref(GAME_LIMIT_SECONDS)
+
+const timeText = computed(() => {
+  const minutes = Math.floor(gameTimer.value / 60)
+  const seconds = (() => {
+    const s = gameTimer.value % 60
+    return s < 10 ? `0${s}` : s
+  })()
+  if (minutes === 0) return `${seconds} 秒`
+  return `${minutes} 分 ${seconds} 秒`
+})
+
+const startGameTimer = () => {
+  gameTimer.value = GAME_LIMIT_SECONDS
+  const timer = setInterval(() => {
+    if (gameTimer.value > 0) {
+      gameTimer.value--
+    } else {
+      clearInterval(timer)
+    }
+  }, 1000)
+}
 
 const getRandomQuestions = () => {
   gameQuestions.value = map(
@@ -43,9 +67,19 @@ const getRandomQuestions = () => {
     }
   )
 }
+
+const clickGameStart = () => {
+  getRandomQuestions()
+  startGameTimer()
+}
 </script>
 <style scoped lang="scss">
 .gm {
+  p {
+    text-align: center;
+    margin: 0;
+  }
+
   p,
   li {
     font-size: 3rem;
